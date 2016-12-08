a25d99948471343d28a5d271db92e724b6c23fdf
Josh W Lewis <josh.w.lewis@gmail.com>
2016-12-08 09:44:39 -0600

    Make all reported metrics gauges

--- spec/lib/pliny/librato/metrics/backend_spec.rb
+++ spec/lib/pliny/librato/metrics/backend_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
 
 RSpec.describe Pliny::Librato::Metrics::Backend do
-  subject(:source)  { "myapp.production" }
-  subject(:backend) { described_class.new(source: source) }
+  subject(:source)     { "myapp.production" }
+  subject(:backend)    { described_class.new(source: source) }
+  let(:async_reporter) { double("AsyncReporter", report: true) }
+
 
   describe "#report_counts" do
-    let(:async_reporter) { double(_report_counts: true) }
-    it "delegates to async._report_counts" do
+    it "delegates to async.report" do
       expect(backend).to receive(:async).and_return(async_reporter)
-      expect(async_reporter).to receive(:_report_counts).once.with(
+      expect(async_reporter).to receive(:report).once.with(
         'pliny.foo' => 1
       )
 
@@ -16,70 +17,42 @@ RSpec.describe Pliny::Librato::Metrics::Backend do
     end
   end
 
-  describe "#async._report_counts" do
-    it "reports a single count to librato" do
-      expect(Librato::Metrics).to receive(:submit).with(
-        'pliny.foo' => { value: 1, type: :counter, source: source }
-      )
-
-      backend.await._report_counts('pliny.foo' => 1)
-    end
-
-    it "reports multiple counts to librato" do
-      expect(Librato::Metrics).to receive(:submit).with(
-        'pliny.foo' => { value: 1, type: :counter, source: source },
-        'pliny.bar' => { value: 2, type: :counter, source: source }
-      )
-
-      backend.await._report_counts('pliny.foo' => 1, 'pliny.bar' => 2)
-    end
-
-    it "reports errors via the error reporter" do
-      error = StandardError.new(message: "Something went wrong")
-      allow(Librato::Metrics).to receive(:submit).and_raise(error)
-      expect(Pliny::ErrorReporters).to receive(:notify).with(error)
-
-      backend.await._report_counts("pliny.boom" => 1)
-    end
-  end
-
   describe "#report_measures" do
-    let(:async_reporter) { double(_report_measures: true) }
-
-    it "delegates to async._report_measures" do
+    it "delegates to async._report" do
       expect(backend).to receive(:async).and_return(async_reporter)
-      expect(async_reporter).to receive(:_report_measures).once.with(
+      expect(async_reporter).to receive(:report).once.with(
         'pliny.foo' => 1.002
       )
 
       backend.report_measures('pliny.foo' => 1.002)
     end
 
+  end
+
+  describe "#report" do
+    it "reports a single count to librato" do
+      expect(Librato::Metrics).to receive(:submit).with(
+        'pliny.foo' => { value: 1, type: :gauge, source: source }
+      )
+
+      backend.report('pliny.foo' => 1)
+    end
+
+    it "reports multiple counts to librato" do
+      expect(Librato::Metrics).to receive(:submit).with(
+        'pliny.foo' => { value: 1, type: :gauge, source: source },
+        'pliny.bar' => { value: 2, type: :gauge, source: source }
+      )
+
+      backend.report('pliny.foo' => 1, 'pliny.bar' => 2)
+    end
+
     it "reports errors via the error reporter" do
       error = StandardError.new(message: "Something went wrong")
       allow(Librato::Metrics).to receive(:submit).and_raise(error)
       expect(Pliny::ErrorReporters).to receive(:notify).with(error)
 
-      backend.await._report_measures("pliny.boom" => 1)
-    end
-  end
-
-  describe "#async._report_measures" do
-    it "reports a single measure to librato" do
-      expect(Librato::Metrics).to receive(:submit).with(
-        'pliny.foo' => { value: 1.002, type: :gauge, source: source }
-      )
-
-      backend.await._report_measures('pliny.foo' => 1.002)
-    end
-
-    it "reports multiple measures to librato" do
-      expect(Librato::Metrics).to receive(:submit).with(
-        'pliny.foo' => { value: 1.5, type: :gauge, source: source },
-        'pliny.bar' => { value: 2.04, type: :gauge, source: source }
-      )
-
-      backend.await._report_measures('pliny.foo' => 1.5, 'pliny.bar' => 2.04)
+      backend.report("pliny.boom" => 1)
     end
   end
 end
