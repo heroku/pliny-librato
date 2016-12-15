d6058358b274f3e62a48975ec4d3ef42e31e1d3c
Josh W Lewis <josh.w.lewis@gmail.com>
2016-12-15 16:17:09 -0600

    Fixups for RuboCop

--- lib/pliny/librato/metrics/backend.rb
+++ lib/pliny/librato/metrics/backend.rb
@@ -1,38 +1,42 @@
 require 'librato/metrics'
 require 'pliny/error_reporters'
 
-module Pliny::Librato
-  module Metrics
-    class Backend
-      attr_reader :queue
+module Pliny
+  module Librato
+    module Metrics
+      # Implements the Pliny::Metrics.backends API. Puts any metrics sent
+      # from Pliny::Metrics onto a queue that gets submitted in batches.
+      class Backend
+        attr_reader :queue
 
-      def initialize(source: nil, interval: 60, count: 1000, queue: nil)
-        @queue = queue || Librato::Metrics::Queue.new(
-          source:              source,
-          autosubmit_interval: interval,
-          autosubmit_count:    count
-        )
-        flush_on_shutdown
-      end
+        def initialize(source: nil, interval: 60, count: 1000, queue: nil)
+          @queue = queue || Librato::Metrics::Queue.new(
+            source:              source,
+            autosubmit_interval: interval,
+            autosubmit_count:    count
+          )
+          flush_on_shutdown
+        end
 
-      def report_counts(counts)
-        report(counts)
-      end
+        def report_counts(counts)
+          report(counts)
+        end
 
-      def report_measures(measures)
-        report(measures)
-      end
+        def report_measures(measures)
+          report(measures)
+        end
 
-      private
+        private
 
-      def report(metrics)
-        queue.add(metrics)
-      rescue => error
-        Pliny::ErrorReporters.notify(error)
-      end
+        def report(metrics)
+          queue.add(metrics)
+        rescue => error
+          Pliny::ErrorReporters.notify(error)
+        end
 
-      def flush_on_shutdown
-        Signal.trap('TERM') { queue.submit }
+        def flush_on_shutdown
+          Signal.trap('TERM') { queue.submit }
+        end
       end
     end
   end
